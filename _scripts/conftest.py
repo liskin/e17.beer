0d596e034bc97cbbc34f3c22f9ef35da71441b03
Tomas Janousek <tomi@nomi.cz>
2026-05-06 15:46:13 +0100

    fix: Hack to avoid 2 second wait after tests

--- _scripts/conftest.py
+++ _scripts/conftest.py
@@ -1,5 +1,16 @@
+import gc
 import os
 
 
 def pytest_configure(config):
     os.environ["GOOGLE_MAPS_API_KEY"] = "__invalid_api_key_for_tests__"
+
+
+def pytest_unconfigure(config):
+    import generate_place_ids
+    import update_places
+
+    # destroy grpc clients to avoid waiting a few seconds for the grpc timer thread to shut down
+    generate_place_ids.client = None
+    update_places.client = None
+    gc.collect()
