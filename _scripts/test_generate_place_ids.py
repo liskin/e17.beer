bbf38b9cf0da83a78f40d26b176bc4a2064d4e84
Tomas Janousek <tomi@nomi.cz>
2026-05-07 13:23:16 +0100

    test: Drop pytest-mock
    Not needed any more - we're no longer monkeypatching.

--- _scripts/test_generate_place_ids.py
+++ _scripts/test_generate_place_ids.py
@@ -1,23 +1,25 @@
+from unittest.mock import MagicMock
+
 import pytest
 
 from generate_place_ids import get_place_data_from_api
 
 
-def test_get_place_data_from_api_ambiguous(mocker):
+def test_get_place_data_from_api_ambiguous():
     # Create mock objects that mimic the Google Library's attributes
-    place_a = mocker.MagicMock()
+    place_a = MagicMock()
     place_a.display_name.text = "Brewery A"
     place_a.id = "123"
 
-    place_b = mocker.MagicMock()
+    place_b = MagicMock()
     place_b.display_name.text = "Brewery B"
     place_b.id = "456"
 
     # Define the return value of the API call
-    mock_response = mocker.MagicMock()
+    mock_response = MagicMock()
     mock_response.places = [place_a, place_b]
 
-    mock_client = mocker.MagicMock()
+    mock_client = MagicMock()
     mock_client.search_text.return_value = mock_response
 
     # We expect an error because result is ambiguous
