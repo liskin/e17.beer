71e03b1bca8c7d5b3d25826261bacbe654e4beb5
Tomas Janousek <tomi@nomi.cz>
2026-05-20 23:57:06 +0100

    style: Drop trailing space

--- hours/_includes/script.js
+++ hours/_includes/script.js
@@ -107,7 +107,7 @@ function getDistance(lat1, lon1, lat2, lon2) {
 	const dLat = (lat2 - lat1) * Math.PI / 180;
 	const dLon = (lon2 - lon1) * Math.PI / 180;
 
-	const a = 
+	const a =
 		Math.sin(dLat / 2) * Math.sin(dLat / 2) +
 		Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
 
